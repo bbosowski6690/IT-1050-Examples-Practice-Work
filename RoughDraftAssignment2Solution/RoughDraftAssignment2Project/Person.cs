86e9bdd4c14d3c7a43be3baabaf23549f07adfbd
Brittany Bosowski <bbosowski6690@outlook.com>
2015-09-22 19:50:49 -0400

    assign2roughdraft

---
+++ RoughDraftAssignment2Solution/RoughDraftAssignment2Project/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoughDraftAssignment2Project
+{
+    class Person
+    {
+        Person Spouse;
+
+        bool IsMarried()
+        {
+            return this.SpouseFirstName != null;
+            return this.SpouseAge != null;
+        }
+        
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public bool IsMarried;
+        Person Spouse;
+        public string SpouseFirstName;
+        public int SpouseAge;
+
+        
+        
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+
+        }
+
+        public string GetSpouseFullName()
+        {
+            return this.SpouseFirstName + " " + this.LastName;
+        }
+
+        public void PrintNameAndAge()
+        {
+            System.Console.WriteLine(this.FirstName + " " + this.LastName + " " + this.Age);
+
+
+        }
+
+        public void PrintSpouseNameAndAge()
+        {
+            System.Console.WriteLine(this.SpouseFirstName + " " + this.LastName + " " + this.SpouseAge);
+
+
+        }
+
+        public void GatherDataFromUser()
+        {
+            this.FirstName = Question.AskForString("What is your first name?");
+            this.LastName = Question.AskForString("What is your last name?");
+            this.Age = Question.AskForInteger("How old are you?");
+            this.IsMarried = Question.AskForBool("Are you married?");
+            this.SpouseFirstName = Question.AskForString("What is your spouse's name?");
+            this.SpouseAge = Question.AskForInteger("How old is your spouse?");
+          
+            
+        }
+
+        public static int Count;
+
+        public static double Average;
+ 
+
+    }
+}
---
+++ RoughDraftAssignment2Solution/RoughDraftAssignment2Project/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoughDraftAssignment2Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double SumOfAllAges = 0;
+            double NumberOfPeople = 0;
+            double AverageAge = SumOfAllAges / NumberOfPeople;
+            int TotalPeople = 0;
+
+            Person person1 = new Person();
+            person1.GatherDataFromUser();
+
+            
+            Person person2 = new Person();
+            person2.GatherDataFromUser();
+
+
+
+            Person person3 = new Person();
+            person3.GatherDataFromUser();
+
+
+            System.Console.WriteLine(person1.PrintNameAndAge());
+            System.Console.WriteLine(person1.PrintSpouseNameAndAge());
+            System.Console.WriteLine(person2.PrintNameAndAge());
+            System.Console.WriteLine(person2.PrintSpouseNameAndAge());
+            System.Console.WriteLine(person3.PrintNameAndAge());
+            System.Console.WriteLine(person3.PrintSpouseNameAndAge());
+            System.Console.WriteLine("Average Age: " + AverageAge);
+            System.Console.WriteLine("Press any key to continue...");
+            System.Console.ReadKey();
+
+        }
+    }
+}
